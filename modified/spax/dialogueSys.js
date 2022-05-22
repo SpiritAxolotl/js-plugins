fe3a8b5c4f8a647091336c3d6ca56f50e30a6901
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2022-05-22 23:44:08 +0800

    Add files via upload

--- modified/spax/dialogueSys.js
+++ modified/spax/dialogueSys.js
@@ -3,7 +3,7 @@
 A JavaScript plugin that adds a div based RPG style dialogue system. Which includes animation, audio, images and function call logic.
 Originally made for SpiritAxolotl's birthday.
 
-Version : 1f - SpaxDay Modified
+Version : 1.3.18f - SpaxDay Modified
 
 By CalmBubbles :)
 
@@ -23,6 +23,8 @@ class dialogueSys
         this.imgSrc = imgSrc;
         this.audioSrc = audioSrc;
         this.audioVol = 0.75;
+        this.textColor = "inherit";
+        this.textBg = "none";
         
         var newDBL = document.createElement("div");
         this.diaBox.appendChild(newDBL);
@@ -32,13 +34,20 @@ class dialogueSys
         
         this.whenTypingFunc = function () { };
         this.notTypingFunc = function () { };
+        
+        this.readyForNextCall = true;
     }
     
     SetActive (state, funcAfter)
     {
+        if (!this.readyForNextCall) return requestAnimationFrame(() => { this.SetActive(state, funcAfter); });
+        
+        this.readyForNextCall = false;
+        
         var animInTime = this.animInTime;
         var animOutTime = this.animOutTime;
         
+        this.charIndex = 0;
         this.SpecialBox = null;
         
         if (funcAfter == null) funcAfter = function () { };
@@ -54,6 +63,8 @@ class dialogueSys
                 this.diaIsActive = true;
                 
                 funcAfter();
+                
+                this.readyForNextCall = true;
             }, animInTime * 1000);
         }
         else
@@ -67,7 +78,10 @@ class dialogueSys
             setTimeout(() => {
                 this.diaBox.setAttribute("data-disabled", "true");
                 this.diaBox.style.animation = "none";
+                
                 funcAfter();
+                
+                this.readyForNextCall = true;
             }, animOutTime * 1000);
         }
     }
@@ -76,17 +90,26 @@ class dialogueSys
     {
         if (!this.diaIsActive) return;
         
-        var index = 0;
-        var interval;
-        var sndSrc;
+        if (!this.readyForNextCall) return requestAnimationFrame(() => { this.typeOnBox(text, speed, person, face, funcAfter); });
+        
+        this.readyForNextCall = false;
+        
+        if (funcAfter == null) funcAfter = function () { };
+        
+        var modifiedText = "";
         var validPeep = false;
+        var sndSrc;
+        var interval;
+        var charIndexOld = this.charIndex;
+        
+        for (let i = 0; i < text.length; i++)
+        {
+            modifiedText += `<span id="dialogueSysChar_${i + this.charIndex}" style="color : ${this.textColor}; background : ${this.textBg}; visibility : hidden;">${text[i]}</span>`;
+        }
         
         for (let i = 0; i < this.peeps.length; i++)
         {
-            if (person == this.peeps[i])
-            {
-                validPeep = true;
-            }
+            if (person == this.peeps[i]) validPeep = true;
         }
         
         if (validPeep != this.SpecialBox)
@@ -100,7 +123,7 @@ class dialogueSys
                     imageVar = ` class="spaxDayECtext-boximg spaxDayEA"`;
                 }
                 
-                this.diaBoxLine.innerHTML = `<img${imageVar} src="${this.imgSrc}/${person}/${face}.png"><span></span>`;
+                this.diaBoxLine.innerHTML = `<img${imageVar} src="${this.imgSrc}/${person}/${face}.png"><span>${modifiedText}</span>`;
                 
                 this.textBox = this.diaBoxLine.querySelector("span");
                 
@@ -108,7 +131,7 @@ class dialogueSys
             }
             else
             {
-                this.diaBoxLine.innerHTML = "<span></span>";
+                this.diaBoxLine.innerHTML = `<span>${modifiedText}</span>`;
                 
                 this.textBox = this.diaBoxLine.querySelector("span");
                 
@@ -122,11 +145,14 @@ class dialogueSys
             if (validPeep)
             {
                 this.diaBoxLine.querySelector("img").src = `${this.imgSrc}/${person}/${face}.png`;
+                this.textBox.innerHTML += modifiedText;
                 
                 sndSrc = `${this.audioSrc}/${person}.ogg`;
             }
             else
             {
+                this.textBox.innerHTML += modifiedText;
+                
                 sndSrc = `${this.audioSrc}/default.ogg`;
             }
         }
@@ -134,55 +160,83 @@ class dialogueSys
         this.whenTypingFunc();
         
         interval = setInterval(() => {
+            if (!document.hasFocus()) return null;
+            
             var textSnd = new Audio(sndSrc);
             textSnd.volume = this.audioVol;
             var shouldPlaySnd = false;
             
-            if (index < text.length)
+            if (this.charIndex < text.length + charIndexOld)
             {
+                let currentChar = document.querySelector(`#dialogueSysChar_${this.charIndex}`);
+                
                 for (let i = 0; i < this.blockedChars.length; i++)
                 {
-                    if (text[index] != this.blockedChars[i])
-                    {
-                        shouldPlaySnd = true;
-                    }
+                    if (currentChar.innerHTML != this.blockedChars[i]) shouldPlaySnd = true;
                 }
                 
-                if (shouldPlaySnd)
-                {
-                    textSnd.play();
-                }
+                if (shouldPlaySnd) textSnd.play();
                 
-                this.textBox.innerHTML += text[index];
+                currentChar.style.visibility = "visible";
                 
-                index++;
+                this.charIndex++;
             }
-            
-            if (index == text.length)
+            else if (this.charIndex == text.length + charIndexOld)
             {
                 clearInterval(interval);
+                
                 this.notTypingFunc();
                 funcAfter();
+                
+                this.readyForNextCall = true;
             }
         }, (24 / speed));
     }
     
     lineBreakBox (amount)
     {
+        if (!this.readyForNextCall) return requestAnimationFrame(() => { this.lineBreakBox(amount); });
+        
+        this.readyForNextCall = false;
+        
         for (let i = 0; i < amount; i++)
         {
             this.textBox.innerHTML += "<br>";
         }
+        
+        this.readyForNextCall = true;
     }
     
     resetBoxTo (text)
     {
-        this.textBox.innerHTML = text;
+        if (!this.readyForNextCall) return requestAnimationFrame(() => { this.resetBoxTo(text); });
+        
+        this.readyForNextCall = false;
+        
+        var modifiedText = "";
+        
+        for (let i = 0; i < text.length; i++)
+        {
+            modifiedText += `<span id="dialogueSysChar_${this.charIndex + i}" style="color : ${this.textColor}; background : ${this.textBg};">${text[i]}</span>`;
+        }
+        
+        this.charIndex = text.length;
+        
+        this.textBox.innerHTML = modifiedText;
+        
+        this.readyForNextCall = true;
     }
     
     clearBox ()
     {
-        this.diaBoxLine.innerHTML = "";
+        if (!this.readyForNextCall) return requestAnimationFrame(() => { this.clearBox(); });
+        
+        this.readyForNextCall = false;
+        
         this.SpecialBox = null;
+        this.charIndex = 0;
+        this.diaBoxLine.innerHTML = "";
+        
+        this.readyForNextCall = true;
     }
 }
