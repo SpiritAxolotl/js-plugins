eab262927316f0369c3c81ff2ad836d0b72bf909
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2022-05-22 16:01:26 +0800

    Add files via upload

--- dialogueSys.js
+++ dialogueSys.js
@@ -3,7 +3,7 @@
 A JavaScript plugin that adds a div based RPG style dialogue system. Which includes animation, audio, images and function call logic.
 Originally made for SpiritAxolotl's birthday.
 
-Version : 1f
+Version : 1.32a
 
 By CalmBubbles :)
 
@@ -23,6 +23,8 @@ class dialogueSys
         this.imgSrc = imgSrc;
         this.audioSrc = audioSrc;
         this.audioVol = 0.75;
+        this.textColor = "white";
+        this.textBg = "none";
         
         var newDBL = document.createElement("div");
         this.diaBox.appendChild(newDBL);
@@ -39,6 +41,7 @@ class dialogueSys
         var animInTime = this.animInTime;
         var animOutTime = this.animOutTime;
         
+        this.charIndex = 0;
         this.SpecialBox = null;
         
         if (funcAfter == null) funcAfter = function () { };
@@ -76,24 +79,26 @@ class dialogueSys
     {
         if (!this.diaIsActive) return;
         
-        var index = 0;
+        var modifiedText;
+        var validPeep = false;
         var interval;
         var sndSrc;
-        var validPeep = false;
+        
+        for (let i = 0; i < text.length; i++)
+        {
+            modifiedText += `<span id="dialogueSysChar_${i + this.charIndex}" style="color : ${this.textColor}; background : ${this.textBg}; visibility : hidden;">${text[i]}</span>`;
+        }
         
         for (let i = 0; i < this.peeps.length; i++)
         {
-            if (person == this.peeps[i])
-            {
-                validPeep = true;
-            }
+            if (person == this.peeps[i]) validPeep = true;
         }
         
         if (validPeep != this.SpecialBox)
         {
             if (validPeep)
             {
-                this.diaBoxLine.innerHTML = `<img src="${this.imgSrc}/${person}/${face}.png"><span></span>`;
+                this.diaBoxLine.innerHTML = `<img src="${this.imgSrc}/${person}/${face}.png"><span>${modifiedText}</span>`;
                 
                 this.textBox = this.diaBoxLine.querySelector("span");
                 
@@ -101,7 +106,7 @@ class dialogueSys
             }
             else
             {
-                this.diaBoxLine.innerHTML = "<span></span>";
+                this.diaBoxLine.innerHTML = `<span>${modifiedText}</span>`;
                 
                 this.textBox = this.diaBoxLine.querySelector("span");
                 
@@ -115,11 +120,14 @@ class dialogueSys
             if (validPeep)
             {
                 this.diaBoxLine.querySelector("img").src = `${this.imgSrc}/${person}/${face}.png`;
+                this.textBox.innerHTML += modifiedText;
                 
                 sndSrc = `${this.audioSrc}/${person}.ogg`;
             }
             else
             {
+                this.textBox.innerHTML += modifiedText;
+                
                 sndSrc = `${this.audioSrc}/default.ogg`;
             }
         }
@@ -131,24 +139,20 @@ class dialogueSys
             textSnd.volume = this.audioVol;
             var shouldPlaySnd = false;
             
-            if (index < text.length)
+            if (this.charIndex < text.length + this.charIndex)
             {
+                this.charIndex++;
+                
+                let currentChar = document.querySelector(`dialogueSysChar_${this.charIndex}`);
+                
                 for (let i = 0; i < this.blockedChars.length; i++)
                 {
-                    if (text[index] != this.blockedChars[i])
-                    {
-                        shouldPlaySnd = true;
-                    }
+                    if (currentChar.innerHTML != this.blockedChars[i]) shouldPlaySnd = true;
                 }
                 
-                if (shouldPlaySnd)
-                {
-                    textSnd.play();
-                }
+                if (shouldPlaySnd) textSnd.play();
                 
-                this.textBox.innerHTML += text[index];
-                
-                index++;
+                currentChar.style.visibility = "visible";
             }
             
             if (index == text.length)
@@ -170,12 +174,22 @@ class dialogueSys
     
     resetBoxTo (text)
     {
-        this.textBox.innerHTML = text;
+        var modifiedText;
+        
+        for (let i = 0; i < text.length; i++)
+        {
+            modifiedText += `<span id="dialogueSysChar_${this.charIndex + i}" style="color : ${this.textColor}; background : ${this.textBg};">${text[i]}</span>`;
+        }
+        
+        this.charIndex += text.length;
+        
+        this.textBox.innerHTML = modifiedText;
     }
     
     clearBox ()
     {
         this.diaBoxLine.innerHTML = "";
         this.SpecialBox = null;
+        this.charIndex = 0;
     }
 }
